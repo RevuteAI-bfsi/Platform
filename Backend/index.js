7238bd6405ef1ce49025d57a0b03a4eec40ea36b
saipavansp <116668519+saipavansp@users.noreply.github.com>
2025-04-20 15:50:56 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -173,7 +173,7 @@ app.use((req, res, next) => {
   
   // Essential CORS headers
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Expose-Headers', 'set-cookie');
   
